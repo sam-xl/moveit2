c29a8811191cef481a5fef0f8baf9f9eeb2814df
SyZbidi <104774720+SyZbidi@users.noreply.github.com>
2024-06-05 09:33:29 +0200

    fix indentation
    Co-authored-by: G.A. vd. Hoorn <g.a.vanderhoorn@tudelft.nl>

--- moveit_configs_utils/moveit_configs_utils/launches.py
+++ moveit_configs_utils/moveit_configs_utils/launches.py
@@ -204,7 +204,7 @@ def generate_move_group_launch(moveit_config):
         DeclareLaunchArgument(
             "capabilities",
             default_value=capabilities,
-       )
+        )
     )
     # inhibit these default MoveGroup capabilities (space separated)
     ld.add_action(DeclareLaunchArgument("disable_capabilities", default_value=""))
